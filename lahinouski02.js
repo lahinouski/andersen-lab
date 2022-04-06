591a5935d26089fd8ccd0aaf9f1cdf9b79f309e5
lahinouski <aliaksandrlahinouski@gmail.com>
2022-04-06 08:07:25 +0300

    Add Symbol.toPrimitive

--- lahinouski02.js
+++ lahinouski02.js
@@ -16,13 +16,12 @@ function makeObjectDeepCopy(objectToClone) {
 
 // Assignment 2:
 function selectFromInterval(numbersArray, indexStart, indexEnd) {
+  const invalidIndexes = !Number.isFinite(indexStart) || !Number.isFinite(indexEnd);
   const invalidArray = !Array.isArray(numbersArray) || !allNumbers(numbersArray);
-  const invalidStartIndex = typeof indexStart !== 'number' || isNaN(indexStart);
-  const invalidEndIndex = typeof indexEnd !== 'number' || isNaN(indexEnd);
 
   if (invalidArray) {
     throw new Error('Not an array with numbers');
-  } else if (invalidStartIndex || invalidEndIndex) {
+  } else if (invalidIndexes) {
     throw new Error('Range indexes are not numbers');
   } else if (indexStart > indexEnd) {
     [indexStart, indexEnd] = [indexEnd, indexStart];
@@ -35,25 +34,20 @@ function allNumbers(arrayToCheck) {
   let areOnlyNumbers = true;
 
   arrayToCheck = Array.from(arrayToCheck);
-  arrayToCheck.forEach((element) => {
-    const notNumber = typeof element !== 'number' || isNaN(element);
-
-    if (notNumber) {
-      areOnlyNumbers = false;
-    }
-  });
+  arrayToCheck.forEach((element) => !Number.isFinite(element) ? areOnlyNumbers = false : null);
 
   return areOnlyNumbers;
 }
 
 
 // Assignment 3:
+// Make Object Iterable:
 const myIterable = {
   from: 2,
   to: 5,
+
   [Symbol.iterator]() {
-    const invalidFromIndex = typeof this.from !== 'number' || isNaN(this.from) || this.from > this.to;
-    const invalidToIndex = typeof this.to !== 'number' || isNaN(this.to);
+    const invalidIndexes = !Number.isFinite(this.from) || !Number.isFinite(this.to) || this.from > this.to;
     const iteratorObject = {
       currentIndex: this.from,
       lastIndex: this.to,
@@ -61,15 +55,35 @@ const myIterable = {
         if (this.currentIndex <= this.lastIndex) {
           return { done: false, value: this.currentIndex++ };
         }
-        
+
         return { done: true };
       }
     };
 
-    if (invalidFromIndex || invalidToIndex) {
+    if (invalidIndexes) {
       throw new Error('Invalid indexes');
     }
 
     return iteratorObject;
   }
 };
+
+for (const value of myIterable) {
+  console.log(value);
+} // 2 3 4 5 
+
+// Convert Object to Primitive:
+const user = {
+  name: "John",
+  money: 1000,
+  
+  [Symbol.toPrimitive](hint) {
+    alert(`hint: ${hint}`);
+    return hint === "string" ? `{name: "${this.name}"}` : this.money;
+  }
+};
+
+alert(user); // hint: string -> {name: "John"}
+alert(+user); // hint: number -> 1000
+alert(user + 500); // hint: default -> 1500
+alert(user); // hint: string -> {name: "John"}
