c2606274dc488dc84e53be23ce5ee9027e9a0e1c
lahinouski <aliaksandrlahinouski@gmail.com>
2022-04-08 12:38:51 +0300

    Refactor error handling

--- lahinouski01.js
+++ lahinouski01.js
@@ -16,16 +16,15 @@ const promptCountFirst = Number(prompt('First number:'));
 const promptCountSecond = Number(prompt('Second number:'));
 
 function calculator(count1, count2) {
-  if (isNaN(count1)) {
-    console.log('Wrong input!');
-  } else if (isNaN(count2)) {
-    console.log('Wrong input!');
-  } else {
-    const adder = count1 + count2;
-    const divisor = count1 / count2;
+  const adder = count1 + count2;
+  const divisor = count1 / count2;
 
-    console.log(`Calculated values: ${adder}, ${isNaN(divisor) ? 0 : divisor}.`);
+  if (isNaN(count1) || isNaN(count2)) {
+    console.log('Wrong input!');
+    return;
   }
+
+  console.log(`Calculated values: ${adder}, ${isNaN(divisor) ? 0 : divisor}.`);
 }
 
 calculator(promptCountFirst, promptCountSecond);
